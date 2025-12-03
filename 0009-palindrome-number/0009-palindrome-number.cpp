1176a7b211a241105a1a9685142d4c1f6ffc6a98
Divyansh Sevta <dsevta@gmail.com>
2025-12-03 23:52:03 +0530

    0009-PALINDROME-NUMBER Stats: Time: 0 ms (100%), Space: 8.5 MB (92.27%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -4,17 +4,14 @@ public:
         if(x<0){
             return false;
         }
-        int rev = 0;
+        long rev = 0;
         int copy = x;
         while(x!=0){
-            if ((rev > INT_MAX / 10) || (rev < INT_MIN / 10)){
-                return 0;
-            } 
             rev*=10;
             rev += x%10;
             x/=10;
         }
-        bool ans = copy==rev?true:false;
-        return ans;
+        // bool ans = copy==rev?true:false;
+        return copy==rev?true:false;
     }
 };
