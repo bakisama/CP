14796e254a5fcf5c51ab79cfefab42d3a864750c
Divyansh Sevta <dsevta@gmail.com>
2025-12-18 00:03:58 +0530

    1013-FIBONACCI-NUMBER Stats: Time: 2 ms (76.4%), Space: 7.6 MB (87.12%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -2,6 +2,16 @@ class Solution {
 public:
     int fib(int n) {
         if (n == 1 || n==0) return n;
-        else return fib(n-1)+fib(n-2);
+        else {
+            int prev2=0;
+            int prev1=1;
+            int curr=-1;
+            for(int i=2;i<=n;i++){
+            curr=prev1+prev2;
+            prev2=prev1;
+            prev1=curr;
+        }
+        return curr;
+        }
     }
 };
